01cf467ea91968ec991a602cb3d89bc0f3aed96d
Kyle Goehner <KyleA.Goehner@gmail.com>
2013-09-04 11:22:40 -0500

    Inital commit

---
+++ Crawler.js
@@ -0,0 +1,33 @@
+var jsdom = require("jsdom");
+var ziprip = require("./modules/ziprip");
+var flr = require("./FileLineReader");
+
+console.log("Starting...");
+
+var file = new flr.FileLineReader("sites.dat", 100);
+
+while (file.hasNextLine()) {
+	try {
+		jsdom.env({
+			url: file.nextLine(),
+			scripts: ["http://code.jquery.com/jquery.js"],
+			done: function (errors, window) {
+				printSite(window.document, window.url);
+			}
+		});
+	}
+	catch (err) {
+		console.log("Error: ", err);
+	}
+}
+
+function printSite(dom, url) {
+	var addresses = ziprip.extract(dom, url);
+	if (addresses.length > 0) {
+		for (var i=0; i<addresses.length; i++) {
+			console.log(addresses[i].title + " : " + addresses[i].postcode);
+		}
+	}
+	else
+		throw new Error(url + " did not return any addresses.");
+}
