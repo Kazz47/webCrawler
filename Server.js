a8ceb054780e287eece2c1ac53ef2374beec9da7
Kyle Goehner <KyleA.Goehner@gmail.com>
2013-10-07 16:00:43 -0500

    Added test printout for modal.

--- Server.js
+++ Server.js
@@ -33,10 +33,26 @@ function getMaxCrawlSize(req, res, next) {
     res.end();
 }
 
+function getModal(req, res, next) {
+    var webpageId;
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.log("Modal (connection): " + err);
+        } else {
+            connection.query("SELECT w.Id, w.Title, u.URL, u.Hash, s.URL, w.Description, w.Parsed FROM Webpage AS w JOIN URL AS u ON u.Id = w.URLId LEFT JOIN URL AS s ON u.SeedId = s.Id WHERE w.Id = ?", webpageId, function(err, rows) {
+                res.write(result[0]);
+            });
+    res.end();
+}
+
 
 var server = restify.createServer();
 server.get("/pageCount", getWebpageCount);
 server.get("/crawlSize", getMaxCrawlSize);
+server.get("/modal", getModal);
 
 server.listen(8080, function() {
         console.log('%s listening at %s', server.name, server.url);
