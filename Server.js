e7890f6dc2e547b7e15e3dc4bcc7b7b9b1bc9cca
Kyle Goehner <KyleA.Goehner@gmail.com>
2013-10-07 14:51:18 -0500

    Added max crawl size.

--- Server.js
+++ Server.js
@@ -1,36 +1,42 @@
 var restify = require('restify');
+var config = require("./config.json");
 var DAO = require("./dao");
+var serviceDAO = new DAO();
+var pool = serviceDAO.pool;
 
 function getWebpageCount(req, res, next) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    var newDAO = new DAO();
-    var pool = newDAO.pool;
     pool.getConnection(function(err, connection) {
         if (err) {
             console.log("Page count (connection): " + err);
-            //callback();
         } else {
             connection.query("SELECT COUNT(*) as c FROM Webpage", function(err, result) {
                 connection.release();
                 if (err) {
                     console.log("Page count: " + erro.code);
-                    //callback();
                 } else {
                     res.write(result[0].c.toString());
                     res.end();
-                    //callback();
                 }
             });
         }
     });
 }
 
+function getMaxCrawlSize(req, res, next) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.write(config.settings.maxCrawlSize.toString());
+    res.end();
+}
+
 
 var server = restify.createServer();
-server.get("/pagecount", getWebpageCount);
-server.head("/pagecount", getWebpageCount);
+server.head("/pageCount", getWebpageCount);
+server.head("/crawlSize", getMaxCrawlSize);
 
 server.listen(8080, function() {
         console.log('%s listening at %s', server.name, server.url);
