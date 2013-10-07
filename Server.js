dfe21da7a40264f4afb1942830619d621b30d966
Kyle Goehner <KyleA.Goehner@gmail.com>
2013-10-07 13:24:38 -0500

    Added sample DB query in server.

--- Server.js
+++ Server.js
@@ -1,8 +1,32 @@
 var restify = require('restify');
+var DAO = require("./dao);
 
 function respond(req, res, next) {
     res.send("hello " + req.params.name);
 }
+
+function getWebpageCount() {
+    var newDAO = new DAO();
+    var pool = newDAO.pool;
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.log("Page count (connection): " + err);
+            //callback();
+        } else {
+            connection.query("SELECT COUNT(*) FROM Webpage", function(err, result) {
+                if (err) {
+                    console.log("Page count: " + erro.code);
+                    connection.release();
+                    //callback();
+                } else {
+                    res.send(result[0]);
+                    //callback();
+                }
+            });
+        }
+    });
+}
+
 
 var server = restify.createServer();
 server.get("/hello/:name", respond);
