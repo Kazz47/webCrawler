b1006c4a21fadd56e22ff06cc380548c77d21224
Kyle Goehner <KyleA.Goehner@gmail.com>
2013-10-28 11:18:17 -0500

    Added stopwords and position indexing.

--- WordAdder.js
+++ WordAdder.js
@@ -20,16 +20,18 @@ WordAdder.prototype.wordSplitter = function(string) {
 WordAdder.prototype.addWords = function(string, webpageId) {
 	if (!string) return;
 	var self = this;
-	
+
 	var words = this.wordSplitter(string.toLowerCase());
-	
+
 	var running = 0;
 	var limit = 2;
-	
+
 	function wordAddLauncher() {
+        var index = 0;
 		while (running < limit && words.length > 0) {
 			var word = words.shift();
-			self.addWord(word, webpageId, function() {
+            index++;
+			self.addWord(word, index, webpageId, function() {
 				running--;
 				if (words.length > 0) {
 					wordAddLauncher();
@@ -41,50 +43,61 @@ WordAdder.prototype.addWords = function(string, webpageId) {
 			running++;
 		}
 	}
-	
+
 	wordAddLauncher();
 }
 
 // Add new word.
-WordAdder.prototype.addWord = function(word, webpageId, callback) {
+WordAdder.prototype.addWord = function(word, index, webpageId, callback) {
 	var self = this;
 	this.pool.getConnection(function(err, connection) {
 		if (err) {
 			console.log("Add word (Connection): " + err);
 			callback();
 		} else {
-			connection.query("SELECT Id FROM Keyword WHERE Word = ?", [word], function(err, result) {
-				if (err) {
-					console.log("Check word existance: " + err.code);
-					connection.release();
-					callback();
-				} else if (result[0]) {
-					connection.release();
-					var wordId = result[0].Id;
-					self.addWordToPage(wordId, webpageId, function() {
-						callback();
-					});
-				} else {
-					var wordObj = {Word: word};
-					connection.query("INSERT INTO Keyword SET ?", wordObj, function(err, result) {
-						connection.release();
-						if (err) {
-							console.log("Add word: " + err.code);
-							callback();
-						} else {
-							var wordId = result.insertId;
-							self.addWordToPage(wordId, webpageId, function() {
-								callback();
-							});
-						}
-					});
-				}
-			});
+            connection.query("SELECT Id FROM Stopword WHERE Word = ?", [word], function(err, result) {
+                if (err) {
+                    console.log("Check stopword: " + erro.code);
+                    connection.release();
+                    callback();
+                } else if (result[0]) {
+                    connection.release();
+                    callback();
+                } else {
+                    connection.query("SELECT Id FROM Keyword WHERE Word = ?", [word], function(err, result) {
+                        if (err) {
+                            console.log("Check word existance: " + err.code);
+                            connection.release();
+                            callback();
+                        } else if (result[0]) {
+                            connection.release();
+                            var wordId = result[0].Id;
+                            self.addWordToPage(wordId, webpageId, function() {
+                                callback();
+                            });
+                        } else {
+                            var wordObj = {Word: word};
+                            connection.query("INSERT INTO Keyword SET ?", wordObj, function(err, result) {
+                                connection.release();
+                                if (err) {
+                                    console.log("Add word: " + err.code);
+                                    callback();
+                                } else {
+                                    var wordId = result.insertId;
+                                    self.addWordToPage(wordId, index, webpageId, function() {
+                                        callback();
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
 		}
 	});
 }
 
-WordAdder.prototype.addWordToPage = function(wordId, webpageId, callback) {
+WordAdder.prototype.addWordToPage = function(wordId, index, webpageId, callback) {
 	this.pool.getConnection(function(err, connection) {
 		if (err) {
 			console.log("Add word (Connection): " + err);
@@ -97,8 +110,17 @@ WordAdder.prototype.addWordToPage = function(wordId, webpageId, callback) {
 					connection.release();
 					callback();
 				} else {
-					connection.release();
-					callback();
+                    var wpObj = {WebpageId: webpageId, KeywordId: wordId, Position: index};
+                    connection.query("INSERT INTO WebapgePosition SET ?", wpObj, function(err, result) {
+                        if (err) {
+                            console.log("Add wp: " + err.code);
+                            connection.release();
+                            callback();
+                        } else {
+                            connection.release();
+                            callback();
+                        }
+                    });
 				}
 			});
 		}
